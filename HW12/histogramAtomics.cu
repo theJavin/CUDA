19f65eca93d96fedea2cd2370ffdbe7f488a1387
theJavin <gavinmcintosh01@gmail.com>
2023-03-20 14:24:15 -0500

    crazyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

--- HW12/histogramAtomics.cu
+++ HW12/histogramAtomics.cu
@@ -153,20 +153,8 @@ __global__ void fillHistogramGPU(float *randomNumbers, int *hist)
 
     while(id < NUMBER_OF_RANDOM_NUMBERS)    
     {
-		/*
-        if(randomNumbers[id] >= 10)
-        {
-            i = int(randomNumbers[id]) / 10;
-            atomicAdd(&hist[i], 1);
-        }
-        else
-        {
-            atomicAdd(&hist[0], 1);
-        }
-        */
 	    i = int(randomNumbers[id])/10;
-		atomicAdd(&hist[i], 1);
-        
+		atomicAdd(&hist[i], 1);      
         id += jump;
     }
 
