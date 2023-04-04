cdd2e3aeba5039ae5695fb4fee251a4e32f9b43e
theJavin <gavinmcintosh01@gmail.com>
2023-04-04 18:27:36 -0500

    oh yeah.... it's all coming together

---
+++ cudaScript.sh
@@ -0,0 +1,8 @@
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install mesa-utils -y
+sudo apt install build-essential -y
+sudo apt install freeglut3-dev -y
+sudo apt install nvidia-cuda-toolkit -y
+sudo apt install git -y
+
